3a18c99c9a378e0dbeac29cbc79c9b1e0b0a451b
DenysSoldatenko <soldatenko911@gmail.com>
2024-12-17 10:00:00 +0300

    Added base templates

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    # path('login/', views.loginPage, name="login"),
+    # path('logout/', views.logoutUser, name="logout"),
+    # path('register/', views.registerPage, name="register"),
+
+    path('', views.home_view, name="home"),
+
+    path('room/<slug:room_slug>/', views.room_view, name="room"),
+    path('create-room/', views.create_room, name="create-room"),
+    path('update-room/<slug:room_slug>/', views.update_room, name="update-room"),
+    path('delete-room/<slug:room_slug>/', views.delete_room, name="delete-room"),
+
+    path('delete-message/<str:pk>/', views.delete_message, name="delete-message"),
+
+    path('profile/<str:pk>/', views.user_profile, name="profile-user"),
+    path('update-user/', views.update_user, name="update-user"),
+
+    # path('topics/', views.topicsPage, name="topics"),
+    # path('activity/', views.activityPage, name="activity"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,56 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404, render
+from core.models import Room, Message, Topic
 
-def home(request):
-    return HttpResponse("Hello, world. You're at the polls home view.")
+def home_view(request):
+    topics = Topic.objects.all()[:5]
+    rooms = Room.objects.all()
+    room_count = rooms.count()
+    room_messages = Message.objects.all()[:3]
 
-def room(request):
-    return HttpResponse("Hello, world. You're at the polls room view.")
+    context = {
+        'rooms': rooms,
+        'topics': topics,
+        'room_count': room_count,
+        'room_messages': room_messages
+    }
+
+    return render(request, 'core/home_page.html', context)
+
+def room_view(request, room_slug):
+    room = get_object_or_404(Room.objects.select_related('topic'), slug=room_slug)
+    room_messages = room.message_set.select_related('user').all()
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', room_slug=room.slug)
+
+    context = {
+        'room': room,
+        'room_messages': room_messages,
+        'participants': participants
+    }
+    return render(request, 'core/room_page.html', context)
+
+def create_room(request):
+    return None
+
+def update_room(request):
+    return None
+
+def delete_room(request):
+    return None
+
+def delete_message(request):
+    return None
+
+def user_profile(request):
+    return None
+
+def update_user(request):
+    return None
